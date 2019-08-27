a6ffe648e5266dec95d68e56e24d85664ef188a8
javadvhd <vahedi.r46@gmail.com>
2019-08-27 23:51:36 +0430

    fix(master): autoplay is added

--- main.js
+++ main.js
@@ -2,19 +2,27 @@
 let PLAYING = false;
 let LOADED = false;
 let rotation = 0;
+let timer = null;
 
 // DOMS and NODES
 const poster = document.getElementById("poster");
 const playerIcon = document.getElementById("player-icon");
 // music
 const audio = new Audio();
-
+audio.addEventListener(
+  "ended",
+  function() {
+    rotation = 0;
+    PLAYING = true;
+    playPause();
+  },
+  false
+);
 // Setets
 const setPoster = src => {
   if (!!src) poster.src = src;
 };
 const setAudio = src => {
-  console.log("setAudio", src);
   if (!!src) audio.src = src;
 };
 const setIconSrc = src => {
@@ -45,11 +53,18 @@ audio.addEventListener("canplaythrough", () => {
   // messageStae.innerText = "";
   playerIcon.setAttribute("data-loaded", "true");
   setIconSrc("assets/icons/play.png");
-  poster.classList.remove("pause");
+  clearInterval(timer);
+  const duration = audio.duration * 1000 || 10000;
+  timer = setInterval(() => {
+    if (PLAYING) rotation += (360 * 100) / duration;
+    document.querySelector(
+      ".player"
+    ).style.transform = `rotate(${rotation}deg)`;
+  }, 100);
 });
 
 // animator
-setInterval(() => {
+timer = setInterval(() => {
   if (PLAYING) rotation += 0.2;
   document.querySelector(".player").style.transform = `rotate(${rotation}deg)`;
 }, 100);
@@ -64,8 +79,6 @@ const onCustomizeValueChange = ({ key, value }) => {
   }
   if (key === "audio") {
     setAudio(value);
-    console.log(value);
-    console.log("audio link set");
   }
 };
 
@@ -73,7 +86,6 @@ const hooks = {
   onCustomizeValueChange,
   wappWillStart(start) {
     W.loadData().then(({ customize }) => {
-      console.log({ customize });
       const { poster, audio } = customize;
       setPoster(poster);
       setAudio(audio);
