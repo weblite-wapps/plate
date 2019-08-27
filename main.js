b98a9242590571203ad5ef5407a325b84d5c307b
javadvhd <vahedi.r46@gmail.com>
2019-08-27 21:59:30 +0430

    fix(master): some bugs fixed

--- main.js
+++ main.js
@@ -1,31 +1,21 @@
-const W = window.W;
-W.setHooks();
 // STATE
 let PLAYING = false;
 let LOADED = false;
 let rotation = 0;
 
 // DOMS and NODES
-const songName = document.querySelector(".info > h2");
-const messageStae = document.querySelector(".info .state");
-const artist = document.querySelector(".info p > span");
 const poster = document.getElementById("poster");
 const playerIcon = document.getElementById("player-icon");
 // music
-const songAudio = document.querySelector("audio");
+const audio = new Audio();
 
 // Setets
-const setName = name => {
-  songName.innerText = name.toString();
-};
-const setaArtist = name => {
-  artist.innerText = name.toString();
-};
 const setPoster = src => {
   if (!!src) poster.src = src;
 };
 const setAudio = src => {
-  if (!!src) songAudio.src = src;
+  console.log("setAudio", src);
+  if (!!src) audio.src = src;
 };
 const setIconSrc = src => {
   playerIcon.src = src;
@@ -35,54 +25,60 @@ const setIconSrc = src => {
   ADJUST SETTING WITH YOUR CUSTOMIZED DATA HERE
 ***************************************************/
 
-setName("تیکه ها");
-setaArtist("کیان پورتراب");
-setPoster(
-  "https://www.tarafdari.com/sites/default/files/contents/user321924/content-sound/kian_pourtorab_-_pieces.jpg"
-);
-setAudio(
-  "https://www.tarafdari.com/sites/default/files/contents/user321924/content-sound/kiyan_pourtorab_tekeha_128_.mp3"
-);
-
 const playPause = () => {
   if (playerIcon.getAttribute("data-loaded") === "false") return;
   PLAYING = !PLAYING;
 
   if (PLAYING) {
     setIconSrc("assets/icons/pause.png");
-    poster.classList.add("playing");
-    songAudio.play();
+    poster.classList.remove("pause");
+    audio.play();
   } else {
     setIconSrc("assets/icons/play.png");
-    poster.classList.remove("playing");
-    songAudio.pause();
+    poster.classList.add("pause");
+    audio.pause();
   }
 };
 
-messageStae.innerText = "processing...";
-
 // audio
-songAudio.addEventListener("canplaythrough", () => {
-  messageStae.innerText = "";
+audio.addEventListener("canplaythrough", () => {
+  // messageStae.innerText = "";
   playerIcon.setAttribute("data-loaded", "true");
   setIconSrc("assets/icons/play.png");
-});
-songAudio.addEventListener("canplay", () => {
-  messageStae.innerText = "downloading...";
-});
-songAudio.addEventListener("loadstart", event => {
-  console.log(event.type);
-});
-songAudio.addEventListener("loadedmetadata", event => {
-  console.log(event.type);
-});
-songAudio.addEventListener("canplay", event => {
-  console.log(event.type);
+  poster.classList.remove("pause");
 });
 
 // animator
-
 setInterval(() => {
   if (PLAYING) rotation += 0.2;
   document.querySelector(".player").style.transform = `rotate(${rotation}deg)`;
 }, 100);
+
+const W = window.W;
+
+const onCustomizeValueChange = ({ key, value }) => {
+  if (key === "poster") {
+    if (value !== "Enter your poster link") {
+      setPoster(value);
+    }
+  }
+  if (key === "audio") {
+    setAudio(value);
+    console.log(value);
+    console.log("audio link set");
+  }
+};
+
+const hooks = {
+  onCustomizeValueChange,
+  wappWillStart(start) {
+    W.loadData().then(({ customize }) => {
+      console.log({ customize });
+      const { poster, audio } = customize;
+      setPoster(poster);
+      setAudio(audio);
+    });
+    start();
+  }
+};
+W.setHooks(hooks);
